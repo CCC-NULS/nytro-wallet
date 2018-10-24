0c4b14d3655043aa861a301cc3883c3eb7bf04de
Moshe Malawach <moshe.malawach@protonmail.com>
2018-10-24 14:28:29 +0200

    relative imports

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '@/components/Dashboard.vue'
-import Login from '@/components/Login.vue'
-import New from '@/components/New.vue'
-import Account from '@/components/Account.vue'
+import Dashboard from '../components/Dashboard.vue'
+import Login from '../components/Login.vue'
+import New from '../components/New.vue'
+import Account from '../components/Account.vue'
 
 Vue.use(Router)
 
